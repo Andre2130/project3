1d90ec501eb760e86dd28ecbcc222c239ad8069c
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-10-15 22:19:02 -0400

    added seeds.js

---
+++ db/seeds.js
@@ -0,0 +1,26 @@
+require('dotenv').config()
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true})
+mongoose.Promise = global.Promise
+
+const { UserModel, AlbumModel, CollectionModel } = require('./schema')
+
+const Andre = new UserModel({
+    userName: 'andre_robinson',
+    password: 'sauceradio'
+  })
+
+const WTC = new AlbumModel({
+    name: 'The Saga continues',
+    description: 'Artist: Wu Tang Clan'
+})
+
+const soul = new CollectionModel({
+    title: 'Hip-Hop',
+    albums: [WTC]
+})
+
+UserModel.remove({})
+.then(() => Andre.save())
+.then(() => console.log('Successful Save'))
+.then(() => mongoose.connection.close())
