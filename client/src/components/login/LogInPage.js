0ae04d8618a6e778f58009b892c9894f069ba52b
Andre Robinson <andrerobinson@Andres-MacBook-Pro.local>
2017-10-19 09:05:40 -0400

    Styled login page

--- client/src/components/login/LogInPage.js
+++ client/src/components/login/LogInPage.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import axios from 'axios'
 import SignUpForm from './SignUpForm'
+import styled from 'styled-components'
+
+const LogInStyles = styled.div`
+text-align:center;
+`
 
 
 class LogInPage extends Component {
 
   render () {
     return (
-      <div>
+      <LogInStyles>
         <h1>Log-In</h1>
         <h3>Already have an account <Link to={`/user`}>Click here</Link></h3>
         <SignUpForm />
-      </div>
+      </LogInStyles>
     )
   }
 }
